438f0e64f40fcdec7288caf9b122a91766877ecf
ChinnyChilla <michaelzheng04@gmail.com>
2023-02-04 17:00:47 -0500

    created Point class

---
+++ Point.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Constructor;
+
+public class Point {
+	public final double x;
+	public final double y;
+	public final double z;
+	Point(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	public double getAverage() {
+		return (x+y+z)/3;
+	}
+	public String toString() {
+		return "(" + x + "," + y + "," + z + ")";
+	} 
+}
