25f1a5e6900e58764e413875404dc5762301481e
ChinnyChilla <michaelzheng04@gmail.com>
2023-02-04 19:18:04 -0500

    inital window class please fix :(

---
+++ Window.java
@@ -0,0 +1,52 @@
+public class Window {
+	private Pixel[][] printArray;
+	// private final int terminalWidth;
+	// private final int terminalHeight;
+	Window(int terminalWidth, int terminalHeight) {
+		// this.terminalHeight = terminalHeight;
+		// this.terminalWidth = terminalWidth;
+
+		this.printArray = new Pixel[terminalWidth][];
+		for (int i=0; i<printArray.length; i++) {
+			printArray[i] = new Pixel[terminalHeight];
+		}
+	}	
+	public void updatePrintArray(int coordX, int coordY, Pixel newPoint) {
+		printArray[coordX][coordY] = newPoint;
+	}
+
+	private void draw(boolean flip) {
+		
+		System.out.print("\u001b[?25l");
+		System.out.print("\u001b[" + printArray.length + "A");
+		for (int i = 0; i < printArray.length; i++) {
+			for (int j = 0; j < printArray[0].length; j++) {
+				Pixel currentPoint = printArray[i][j];
+				if (currentPoint.notBackground) {
+					// ALlows the flicking of the things we want to draw
+					if (flip) {
+						System.out.print(currentPoint.color + currentPoint.shade
+								+ "\u2588");
+					} else {
+						System.out.print(currentPoint.color + currentPoint.shade + "");
+					}
+					
+				} else {
+					System.out.print(currentPoint.color + currentPoint.shade + "\u2588");
+				}
+			}
+			System.out.println();
+		}
+	}
+	public void print() {
+		boolean flip = false;
+		while (true) {
+			flip = !flip;
+			this.draw(flip);
+		}
+	}
+	public Pixel[][] getPrintArray() {
+		return printArray;
+	}
+	
+}
